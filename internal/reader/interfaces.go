d3841513ab24d89eb1c1eaeec6e1ebe035c4192a
lastdarknes <tim.m@creek.art>
2024-10-24 23:57:19 +0500

    update reader int

--- internal/reader/interfaces.go
+++ internal/reader/interfaces.go
@@ -3,5 +3,6 @@ package reader
 import "github.com/feature-MaybeCoder/go-brocker/internal/models"
 
 type MessagesReader interface {
-	ReadMessagesGroup() models.MessagesGroup
+	readMessage() models.MessagesGroup
+	RunReadingLoop()
 }
