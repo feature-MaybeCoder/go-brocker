4ac5216d2218fce954e78823660331eb9feaa0cc
lastdarknes <tim.m@creek.art>
2024-11-06 21:53:33 +0500

    add QueuesManager interface

--- internal/queue/interfaces.go
+++ internal/queue/interfaces.go
@@ -6,3 +6,10 @@ type Queue interface {
 	PushMessage(models.Message) error
 	Pop() models.Message
 }
+
+type QueuesManager interface {
+	CreateQueue(name string) (Queue, error)
+	GetQueue(name string) (Queue, bool)
+	RecoverQueues() error
+	persistQueue(Queue) error
+}
